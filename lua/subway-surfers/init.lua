eeaf3dbd6a286a4ef859467c3243a8e6b74d42da
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2024-10-26 22:00:03 +0100

    Add bare-bones SubwaySurfers command

--- lua/subway-surfers/init.lua
+++ lua/subway-surfers/init.lua
@@ -1,7 +1,56 @@
 local M = {}
 
 function M.setup()
-	vim.notify("Hello from subway-surfers.nvim!", vim.log.levels.INFO)
+	vim.api.nvim_create_user_command("SubwaySurfers", function()
+		M.open()
+	end, {})
+end
+
+function M.open()
+	if vim.fn.executable("mpv") == 0 then
+		vim.notify("mpv is not installed!", vim.log.levels.ERROR, { title = "Subway Surfers" })
+		return
+	end
+
+	local old_win = vim.api.nvim_get_current_win()
+	vim.cmd("vertical botright split")
+	local win = vim.api.nvim_get_current_win()
+
+	local width = math.floor(vim.o.columns * 0.25)
+	vim.cmd(string.format("vertical resize %i", width))
+
+	local buf = vim.api.nvim_create_buf(false, true)
+
+	vim.api.nvim_set_current_buf(buf)
+	vim.bo[buf].modifiable = false
+	vim.wo[win].number = false
+	vim.wo[win].relativenumber = false
+	vim.wo[win].signcolumn = "no"
+
+	vim.api.nvim_win_set_buf(win, buf)
+
+	local mpv_cmd = {
+		"mpv",
+		"--really-quiet",
+		"--vo=tct",
+		"~/Downloads/subway-surfers.mp4",
+	}
+
+	local shell = vim.fn.has("win32") == 0 and "sh" or "powershell"
+	local cmd = { shell, "-c", vim.iter(mpv_cmd):join(" ") }
+
+	vim.fn.termopen(cmd, {
+		stderr_buffered = true,
+		on_stderr = function(_, data, _)
+			local stderr = vim.iter(data):join("\n")
+
+			vim.notify(stderr, vim.log.levels.ERROR, { title = "Subway Surfers" })
+		end,
+	})
+
+	vim.api.nvim_buf_set_name(buf, "Subway Surfers")
+
+	vim.api.nvim_set_current_win(old_win)
 end
 
 return M
